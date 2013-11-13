624a1326aea680acb5d7539f3c3458300bc41582
Scott Walters <srwalte2@illinois.edu>
2013-11-13 13:37:30 -0600

    Moved get random MIDI to helper function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-var pentatonic_freq = [261.63, 293.67, 329.63, 392.00, 440.00, 523.25]
+var MIN_MIDI = 40;
+var MAX_MIDI = 89;
+var OCTAVE_LENGTH = 12;
+var NUMBER_OF_OCTAVES = 11;
+
+function getRandomPentatonicMIDI(){
+	var base_scale = [0, 2, 4, 7, 9];
+	var octave = Math.floor(Math.random() * NUMBER_OF_OCTAVES);
+	var base_note = base_scale[Math.floor(Math.random() * base_scale.length)];
+	var note = octave * OCTAVE_LENGTH + base_note;
+	if (note < MIN_MIDI || note > MAX_MIDI){
+		return getRandomPentatonicMIDI();
+	}
+	return note;
+}
 
 $(document).ready(function(){
 	$(document).keypress(function(){
 		console.log("Received keypress()");
-		var idx = Math.floor(Math.random() * pentatonic_freq.length);
-		var freq = pentatonic_freq[idx];
+		var freq = getRandomPentatonicMIDI().midicps();
 		var note = T("sin", {
 			freq : freq,
-			mul  : 0.25});
+			mul  : 0.10});
 		note.play();
 		window.setTimeout(function(){
 			note.pause();
