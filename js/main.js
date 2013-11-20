eb3b19a04bded8f6592f81d5c0a8487fbb4262b3
Scott Walters <srwalte2@illinois.edu>
2013-11-20 00:23:51 -0600

    Switch from processing.js to GameJs
    Also, press shift to change keys/colors

---
+++ js/main.js
@@ -0,0 +1,127 @@
+var gamejs = require('gamejs');
+var sound = require('sound');
+
+var spring = 0.05;
+var gravity = 0.05;
+var friction = -0.9;
+var width = 600;
+var height = 400;
+
+function Circle(xin, yin, din, idin, oin, colorin){
+	this.x = xin;
+	this.y = yin;
+	this.diameter = din;
+	this.vx = 0;
+	this.vy = 0;
+	this.id = idin;
+	this.others = oin;
+	this.color = colorin;
+
+
+	this.collide = function(){
+		for (var i = this.id + 1; i < this.others.length; i++) {
+			if (this.others[i]) {
+				var dx = this.others[i].x - this.x;
+				var dy = this.others[i].y - this.y;
+				var distance = Math.sqrt(dx * dx + dy * dy);
+				var minDist = this.others[i].diameter / 2 + this.diameter / 2;
+				if (distance < minDist) {
+					var angle = Math.atan2(dy, dx);
+					var targetX = this.x + Math.cos(angle) * minDist;
+					var targetY = this.y + Math.sin(angle) * minDist;
+					var ax = (targetX - this.others[i].x) * spring;
+					var ay = (targetY - this.others[i].y) * spring;
+					this.vx -= ax;
+					this.vy -= ay;
+					this.others[i].vx += ax;
+					this.others[i].vy += ay;
+				}
+			}
+		}
+	}
+
+	this.move = function(){
+		this.vy += gravity;
+		this.x += this.vx;
+		this.y += this.vy;
+		/*		if (this.x + this.diameter / 2 > width) {
+		 this.x = width - this.diameter / 2;
+		 this.vx *= friction;
+		 }
+		 else if (this.x - this.diameter / 2 < 0) {
+		 this.x = this.diameter / 2;
+		 this.vx *= friction;
+		 }
+		 if (this.y + this.diameter / 2 > height) {
+		 this.y = height - this.diameter / 2;
+		 this.vy *= friction;
+		 }
+		 else if (this.y - this.diameter / 2 < 0) {
+		 this.y = this.diameter / 2;
+		 this.vy *= friction;
+		 }*/
+	}
+
+	this.draw = function(display){
+		gamejs.draw.circle(display, this.color, [this.x, this.y],
+			this.diameter / 2);
+	}
+}
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+var MAX_BALLS = 50;
+
+gamejs.preload([]);
+
+gamejs.ready(function(){
+	var balls = new Array(MAX_BALLS);
+	var ballIdx = 0;
+	var color = "#ffffff";
+
+	var display = gamejs.display.setMode([width, height]);
+
+	gamejs.onEvent(function(event){
+		// event handling
+		if (event.type == 1) {
+			if (event.key == 16) {
+				sound.changeKey();
+				color = get_random_color();
+			}
+			else {
+				sound.playNote();
+				createBall();
+			}
+		}
+	});
+
+	function createBall(){
+		var x = Math.floor(Math.random() * width);
+		var y = Math.floor(Math.random() * height);
+		var diameter = Math.floor(Math.random() * 40) + 30;
+		balls[ballIdx] = new Circle(x, y, diameter, ballIdx, balls, color);
+		ballIdx = (ballIdx + 1) % balls.length;
+	}
+
+	// http://stackoverflow.com/a/1484514/1222411
+	function get_random_color(){
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++) {
+			color += letters[Math.round(Math.random() * 15)];
+		}
+		return color;
+	}
+
+	gamejs.onTick(function(msDuration){
+		gamejs.draw.rect(display, "#000000",
+			new gamejs.Rect([0, 0], [width, height]));
+		for (var i = 0; i < balls.length; i++) {
+			if (balls[i]) {
+				balls[i].collide();
+				balls[i].move();
+				balls[i].draw(display);
+			}
+		}
+	});
+});
--- js/sound.js
+++ js/sound.js
@@ -3,11 +3,12 @@ var MAX_MIDI = 89;
 var OCTAVE_LENGTH = 12;
 var NUMBER_OF_OCTAVES = 11;
 var SOUND_LENGTH = 1000; //ms
+var key = 0;
 
 function getRandomPentatonicMIDI(){
 	var base_scale = [0, 2, 4, 7, 9];
 	var octave = Math.floor(Math.random() * NUMBER_OF_OCTAVES);
-	var base_note = base_scale[Math.floor(Math.random() * base_scale.length)];
+	var base_note = base_scale[Math.floor(Math.random() * base_scale.length)] + key;
 	var note = octave * OCTAVE_LENGTH + base_note;
 	if(note < MIN_MIDI || note > MAX_MIDI) {
 		return getRandomPentatonicMIDI();
@@ -24,3 +25,11 @@ function playNote(){
 		this.pause();
 	}).bang().play();
 }
+
+function changeKey(){
+	key = key + Math.random() * OCTAVE_LENGTH;
+	key = key % 12;
+}
+
+exports.playNote = playNote;
+exports.changeKey = changeKey;
